ddf9031e74294b5c6df006be754d77f239cf0060
wong561 <88598378+wong561@users.noreply.github.com>
2023-01-02 16:19:50 -0500

    first

---
+++ ts-basics/basics.ts
@@ -0,0 +1,40 @@
+//Primitives:number, string, boolean
+//More complex types: arrays, objects
+//Function types, parameters
+
+//Primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+
+userName: "max"
+
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ["sport", "Jiu Jitsu"];
+type Person = {
+    name: string;
+    age: number;
+}
+
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: "Max",
+    age: 32,
+
+};
+
+let people: Person[];
+
+let course = "React - The Complete Guide";
+
